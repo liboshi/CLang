f24dd0319ab55b9dd75eb9949a47f8d0053029d0
Boush Li <boush.1.li@nokia.com>
2013-10-28 21:58:10 +0800

    o Add reverse_string in swap.c.

--- swap.c
+++ swap.c
@@ -7,10 +7,28 @@ void swap_int(int *x, int *y)
         *x = tmp;
 }
 
+void reverse_string(char *str)
+{
+        char *last_char;
+        for(last_char = str; *last_char != '\0'; last_char++)
+                ;
+        last_char--;
+        while(str < last_char)
+        {
+                char tmp;
+                tmp = *str;
+                *str++ = *last_char;
+                *last_char-- = tmp;
+        }
+}
+
 int main(int argc, char *argv[])
 {
         int i = 1, j = 2;
         swap_int(&i, &j);
         printf("i = %d, j = %d\n", i, j);
+        char *str = "string";
+        reverse_string(str);
+        printf("Reversed string: %s", str);
         return 0;
 }
