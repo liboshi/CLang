010fe7d6a83970aa7c311534bb8b93d2caa51848
Boush Li <li.boush@gmail.com>
2013-11-02 20:51:07 +0800

    o Change code style.

--- deamon-demo.c
+++ deamon-demo.c
@@ -9,52 +9,52 @@
 
 int main(int argc, char* argv[])
 {
-    FILE *fp = NULL;
-    pid_t process_id = 0;
-    pid_t sid = 0;
-    int count = 0;
-    
-    // Create child process
-    process_id = fork();
-    if (process_id < 0)
-    {
-        printf("fork failed!\n");
-        exit(1);
-    }
+        FILE *fp = NULL;
+        pid_t process_id = 0;
+        pid_t sid = 0;
+        int count = 0;
 
-    if (process_id > 0)
-    {
-        printf("process_id of child process %d\n", process_id);
-        exit(0);
-    }
-
-    // unmask the file mode
-    umask(0);
-    sid = setsid();
-    if (sid < 0)
-    {
-        exit(1);
-    }
-
-    chdir("C:\\Boush");
-
-    // Close stdin, stdout and stderr
-    close(STDIN_FILENO);
-    close(STDOUT_FILENO);
-    close(STDERR_FILENO);
-    fp = fopen("Log.txt", "w+");
-    while (1)
-    {
-        count++;
-        sleep(1);
-        fprintf(fp, "=== Logging info...\n");
-        fflush(fp);
-        if (25 == count)
+        // Create child process
+        process_id = fork();
+        if (process_id < 0)
         {
-            printf("=== deamon process stop...\n");
-            exit(0);
+                printf("fork failed!\n");
+                exit(1);
         }
-    }
-    fclose(fp);
-    return 0;
+
+        if (process_id > 0)
+        {
+                printf("process_id of child process %d\n", process_id);
+                exit(0);
+        }
+
+        // unmask the file mode
+        umask(0);
+        sid = setsid();
+        if (sid < 0)
+        {
+                exit(1);
+        }
+
+        chdir("C:\\Boush");
+
+        // Close stdin, stdout and stderr
+        close(STDIN_FILENO);
+        close(STDOUT_FILENO);
+        close(STDERR_FILENO);
+        fp = fopen("Log.txt", "w+");
+        while (1)
+        {
+                count++;
+                sleep(1);
+                fprintf(fp, "=== Logging info...\n");
+                fflush(fp);
+                if (25 == count)
+                {
+                        printf("=== deamon process stop...\n");
+                        exit(0);
+                }
+        }
+        fclose(fp);
+        return 0;
 }
--- thread-demo.c
+++ thread-demo.c
@@ -9,61 +9,61 @@
 
 void *print_hello(void *threadid)
 {
-    long tid;
-    tid = (long)threadid;
-    printf("Hello world! This is thread #%ld\n", tid);
-    pthread_exit(NULL);
+        long tid;
+        tid = (long)threadid;
+        printf("Hello world! This is thread #%ld\n", tid);
+        pthread_exit(NULL);
 }
 
 void *busy_work(void *t)
 {
-    int i;
-    long tid;
-    double result = 0.0;
-    tid = (long)t;
-    printf("Thread %ld starting...\n", tid);
-    for (i = 0; i < 10000000; i++)
-    {
-        result = result + sin(i) * tan(i);
-    }
-    printf("Thread %ld done. | Result = %e\n", tid, result);
-    pthread_exit((void *) t);
+        int i;
+        long tid;
+        double result = 0.0;
+        tid = (long)t;
+        printf("Thread %ld starting...\n", tid);
+        for (i = 0; i < 10000000; i++)
+        {
+                result = result + sin(i) * tan(i);
+        }
+        printf("Thread %ld done. | Result = %e\n", tid, result);
+        pthread_exit((void *) t);
 }
 
 int main(int argc, char *argv[])
 {
-    pthread_t threads[NUM_THREADS];
-    pthread_attr_t attr;
-    int rc;
-    long t;
-    void *status;
+        pthread_t threads[NUM_THREADS];
+        pthread_attr_t attr;
+        int rc;
+        long t;
+        void *status;
 
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+        pthread_attr_init(&attr);
+        pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
-    for (t = 0; t < NUM_THREADS; t++)
-    {
-        printf("Main: Creating thread %ld\n", t);
-        rc = pthread_create(&threads[t], &attr, busy_work, (void *)t);
-        if (rc)
+        for (t = 0; t < NUM_THREADS; t++)
         {
-            printf("Error; return code from pthread_create() is %d\n", rc);
-            exit(-1);
+                printf("Main: Creating thread %ld\n", t);
+                rc = pthread_create(&threads[t], &attr, busy_work, (void *)t);
+                if (rc)
+                {
+                        printf("Error; return code from pthread_create() is %d\n", rc);
+                        exit(-1);
+                }
+                printf("Main: Completed join with thread %ld having a status of %ld\n", t, (long)status);
         }
-        printf("Main: Completed join with thread %ld having a status of %ld\n", t, (long)status);
-    }
-    /*
-    for (t = 0; t < NUM_THREADS; t++)
-    {
-        printf("In main: creating thread #%ld\n", t);
-        rc = pthread_create(&threads[t], NULL, print_hello, (void *)t);
-        if (rc)
-        {
-            printf("Error; return code from pthread_create() is %d\n", rc);
-            exit(-1);
-        }
-    }
-    */
-    printf("Main: program finished.... exiting.\n");
-    pthread_exit(NULL);
+        /*
+           for (t = 0; t < NUM_THREADS; t++)
+           {
+           printf("In main: creating thread #%ld\n", t);
+           rc = pthread_create(&threads[t], NULL, print_hello, (void *)t);
+           if (rc)
+           {
+           printf("Error; return code from pthread_create() is %d\n", rc);
+           exit(-1);
+           }
+           }
+           */
+        printf("Main: program finished.... exiting.\n");
+        pthread_exit(NULL);
 }
