4ef076f01220b06a34aa0be067e132621f820cfc
Li Boshi <li.boush@gmail.com>
2014-11-21 16:02:07 +0800

    o Add file reader, a little ugly...

---
+++ file-read.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+        FILE *fp;
+        char c;
+        char buff[1024 * 1024];
+        int i = 0;
+        fp = fopen(argv[1], "r");
+        while (!feof(fp)) {
+                c = fgetc(fp);
+                buff[i++] = c;
+        }
+        fclose(fp);
+        buff[i - 1] = '\0';
+        for (int j = 0;;j++) {
+                if (buff[j] != '\0') {
+                        printf("%c", buff[j]);
+                } else{
+                        break;
+                }
+        }
+}
