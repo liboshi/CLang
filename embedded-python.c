8e08e46d3d7af272e845f35b228543be10d408a3
Boush Li <li.boush@gmail.com>
2014-12-01 16:40:06 +0800

    o Update embedded python.

--- embedded-python.c
+++ embedded-python.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[])
 {
-        PyObject *pName, *pModule, *pDict, *pFunc, *pValue;
+        PyObject *pName, *pModule, *pDict, *pFunc;
 
         if (argc < 3) {
                 printf("Usage: exe_name python_source func_name\n");
@@ -10,14 +10,27 @@ int main(int argc, char *argv[])
         }
 
         // Initialize python interpreter
+        printf("=======");
+        exit;
         Py_Initialize();
+        if (!Py_IsInitialized()) {
+                printf("Initialize python failed...");
+        } else {
+                printf("Initialize successfully...");
+        }
 
         pName = PyString_FromString(argv[1]);
         pModule = PyImport_Import(pName);
+        if (pModule) {
+                printf("Load module successfully...");
+        } else {
+                printf("Load module failed..");
+                exit;
+        }
         pDict = PyModule_GetDict(pModule);
         pFunc = PyDict_GetItemString(pDict, argv[2]);
 
-        if (pyCallable_Check(pFunc)) {
+        if (PyCallable_Check(pFunc)) {
                 PyObject_CallObject(pFunc, NULL);
         } else {
                 PyErr_Print();
