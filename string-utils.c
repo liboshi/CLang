7caecccfba42b82f92616b52ea02564e4c001418
Boush Li <li.boush@gmail.com>
2014-12-02 11:11:42 +0800

    o Add test for memcmp.

--- string-utils.c
+++ string-utils.c
@@ -14,9 +14,24 @@ int memchr_test()
         return 0;
 }
 
+int memcmp_test()
+{
+        char buffer1[] = "DWgaOtP12df0";
+        char buffer2[] = "DWGAOTP12DF0";
+        int n;
+        n=memcmp(buffer1, buffer2, sizeof(buffer1));
+
+        if (n>0) printf ("'%s' is greater than '%s'.\n",buffer1,buffer2);
+        else if (n<0) printf ("'%s' is less than '%s'.\n",buffer1,buffer2);
+        else printf ("'%s' is the same as '%s'.\n",buffer1,buffer2);
+
+        return 0;
+}
+
 int main()
 {
         int ret;
-        ret = memchr_test();
+        //ret = memchr_test();
+        ret = memcmp_test();
         return ret;
 }
