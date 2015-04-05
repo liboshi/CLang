34027c496e103e8009ba8055886d2a72064cdfae
Boush Li <li.boush@gmail.com>
2015-04-06 00:01:27 +0800

    o Add test for vector.

---
+++ cpp/vector-test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+        vector<string> v;
+        ifstream in("vector-test.cpp");
+        string line;
+        while(getline(in, line))
+                v.push_back(line);
+        for(int i = 0; i < v.size(); i++)
+                cout << i << ": " << v[i] << endl;
+}
