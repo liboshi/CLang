5f9f118d60699fb86494fc7f836659cd7b8e7949
Boush Li <li.boush@gmail.com>
2015-04-06 11:33:42 +0800

    o Update vector-test.cpp. o Add Makefile.

--- cpp/vector-test.cpp
+++ cpp/vector-test.cpp
@@ -5,7 +5,18 @@
 
 using namespace std;
 
-int main() {
+int get_words() {
+        vector<string> words;
+        string word;
+        ifstream in("vector-test.cpp");
+        while(in >> word)
+                words.push_back(word);
+        for(int i = 0; i < words.size(); i++)
+                cout << words[i] << endl;
+        return 0;
+}
+
+int get_line() {
         vector<string> v;
         ifstream in("vector-test.cpp");
         string line;
@@ -13,4 +24,19 @@ int main() {
                 v.push_back(line);
         for(int i = 0; i < v.size(); i++)
                 cout << i << ": " << v[i] << endl;
+        return 0;
+}
+
+int get_int() {
+        vector<int> v;
+        for(int i = 0; i < 10; i++)
+                v.push_back(i);
+        for(int i = 0; i < v.size(); i++)
+                cout << v[i] << endl;
+        return 0;
+}
+
+int main() {
+        get_int();
+        return 0;
 }
