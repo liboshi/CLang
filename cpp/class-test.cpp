45337e0d2072cdeaeb35b081f88a0f744eb4226e
Boush Li <li.boush@gmail.com>
2015-04-12 22:42:18 +0800

    o Update test for class.

--- cpp/class-test.cpp
+++ cpp/class-test.cpp
@@ -7,8 +7,16 @@ private:
         int i;
 public:
         int j;
+        void println();
 protected:
         int k;
+};
+
+void A::println() {
+        cout << "Function println() is callled." << endl;
+}
+
+class B : public A {
 };
 
 class Tree {
@@ -36,8 +44,29 @@ void Tree::printsize() {
         cout << "Tree height is " << height << endl;
 }
 
+class Treea : public Tree {
+        int height;
+public:
+        Treea(int initHeight);
+        ~Treea();
+};
+
+Treea::Treea(int initHeight) : Tree(initHeight) {
+        height = initHeight;
+        cout << "Hello class Treea." << endl;
+}
+
+Treea::~Treea() {
+        cout << "Inside Treea destructor" << endl;
+}
+
 int main() {
         Tree t(12);
         t.printsize();
         t.grow(4);
+        B b;
+        b.println();
+        Treea ta(10);
+        ta.grow(5);
+        ta.printsize();
 }
