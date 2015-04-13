2afec6493a0892e4fc1b22edaf584625a355870b
Boush Li <li.boush@gmail.com>
2015-04-13 22:06:53 +0800

    o Update test for class. o Rename pointer_func_test.cpp to pointer-func-test.cpp.

--- cpp/class-test.cpp
+++ cpp/class-test.cpp
@@ -1,6 +1,11 @@
- #include <iostream>
+#include <iostream>
+#include <cassert>
+#include <fstream>
+#include <string>
+#include "class-test.h"
 
 using namespace std;
+int increment = 100;
 
 class A {
 private:
@@ -60,13 +65,83 @@ Treea::~Treea() {
         cout << "Inside Treea destructor" << endl;
 }
 
+
+Stash::Stash(int sz) {
+        size = sz;
+        quantity = 0;
+        next = 0;
+        storage = 0;
+}
+
+Stash::Stash(int sz, int initQuantity) {
+        size = sz;
+        quantity = 0;
+        next = 0;
+        storage = 0;
+        inflate(initQuantity);
+}
+
+Stash::~Stash() {
+        if(storage != 0) {
+                cout << "freeing storage" << endl;
+                delete []storage;
+        }
+}
+
+int Stash::add(void* element) {
+        if(next >= quantity)
+                inflate(increment);
+        int startBytes = next * size;
+        unsigned char* e = (unsigned char*)element;
+        for(int i = 0; i < size; i++)
+                storage[startBytes + 1] = e[i];
+        next++;
+        return(next - 1);
+}
+
+void* Stash::fetch(int index) {
+        if(0 <= index)
+                cout << "Stash::fetch (-) index" << endl;
+        if(index >= next)
+                return 0;
+        return &(storage[index * size]);
+
+}
+
+int Stash::count() {
+        return next;
+}
+
+void Stash::inflate(int increase) {
+        assert(increase >= 0);
+        if(increase == 0) return;
+        int newQuantity = quantity + increase;
+        int newBytes = newQuantity * size;
+        int oldBytes = quantity * size;
+        unsigned char* b = new unsigned char[newBytes];
+        for(int i = 0; i < oldBytes; i++)
+                b[i] = storage[i];
+        delete [](storage);
+        storage = b;
+        quantity = newQuantity;
+}
+
 int main() {
-        Tree t(12);
-        t.printsize();
-        t.grow(4);
-        B b;
-        b.println();
-        Treea ta(10);
-        ta.grow(5);
-        ta.printsize();
+        Stash intStash(sizeof(int));
+        for(int i = 0; i < 10; i++)
+                intStash.add(&i);
+        for(int j = 0; j < intStash.count(); j++)
+                cout << "intStash.fetch(" << j << ") = " << *(int*)intStash.fetch(j)
+                     << endl;
+        const int bufsize = 10;
+        Stash stringStash(sizeof(char) * bufsize, 10);
+        ifstream in("class-test.cpp");
+        string line;
+        while(getline(in, line))
+                stringStash.add((char*) line.c_str());
+        int k = 0;
+        char* cp;
+        while((cp = (char *)stringStash.fetch(k++)) != 0)
+                cout << "stringStash.fetch(" << k << ") = "
+                    << cp << endl;
 }
