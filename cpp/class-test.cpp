12f0204439a3d740990114eaa3e2ae0a0b013877
Boush Li <li.boush@gmail.com>
2015-04-14 22:42:11 +0800

    o Const.

--- cpp/class-test.cpp
+++ cpp/class-test.cpp
@@ -126,22 +126,12 @@ void Stash::inflate(int increase) {
         quantity = newQuantity;
 }
 
+X::X(int ii) : i(ii) {}
+int X::f() const { return i; }
+
 int main() {
-        Stash intStash(sizeof(int));
-        for(int i = 0; i < 10; i++)
-                intStash.add(&i);
-        for(int j = 0; j < intStash.count(); j++)
-                cout << "intStash.fetch(" << j << ") = " << *(int*)intStash.fetch(j)
-                     << endl;
-        const int bufsize = 10;
-        Stash stringStash(sizeof(char) * bufsize, 10);
-        ifstream in("class-test.cpp");
-        string line;
-        while(getline(in, line))
-                stringStash.add((char*) line.c_str());
-        int k = 0;
-        char* cp;
-        while((cp = (char *)stringStash.fetch(k++)) != 0)
-                cout << "stringStash.fetch(" << k << ") = "
-                    << cp << endl;
+        X x1(10);
+        const X x2(20);
+        x1.f();
+        x2.f();
 }
---
+++ cpp/class-test.h
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class Stash {
+        int size;
+        int quantity;
+        int next;
+        unsigned char* storage;
+        void inflate(int increase);
+public:
+        Stash(int size);
+        Stash(int size, int initQuantity);
+        ~Stash();
+        int add(void* element);
+        void* fetch(int index);
+        int count();
+};
+
+class X {
+        int i;
+public:
+        X(int ii);
+        int f() const;
+};
