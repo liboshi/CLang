63ed36a8b0fe84cbfe698facdeb015f8e63e005b
Boush Li <li.boush@gmail.com>
2015-04-20 13:34:03 +0800

    o Update.

--- atoi-test.c
+++ atoi-test.c
@@ -4,10 +4,13 @@
 
 int main() {
         int i;
+        int j;
         char buffer[256];
         printf("Enter a number: ");
         fgets(buffer, 256, stdin);
         i = atoi(buffer);
+        j = atoi("123456");
         printf("The value entered is %d. Its double is %d\n", i, i * 2);
+        printf("The value of j is %d\n", j);
         return 0;
 }
