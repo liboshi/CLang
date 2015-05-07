1ea51443c7d48c47863b65a36b86e9c5cc9455a2
Boush Li <li.boush@gmail.com>
2015-05-07 21:53:35 +0800

    o Update process-bar.c

--- process-bar.c
+++ process-bar.c
@@ -6,10 +6,11 @@ int main(int argc, char *argv[])
 {
         unsigned len = 60;
         char *bar = (char *)malloc(sizeof(char) * (len + 1));
-        for (int i = 0; i < len + 1; ++i) {
+        int i;
+        for (i = 0; i < len + 1; ++i) {
                 bar[i] = '#';
         }
-        for (int i = 0; i < len; ++i) {
+        for (i = 0; i < len; ++i) {
                 printf("process:[%s]%d%\r", bar + len - i, i + 1);
                 fflush(stdout);
                 usleep(100000);
