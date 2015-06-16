ac1e6275bb6727718e344825a060b51468fb2d7b
Boush Li <li.boush@gmail.com>
2015-06-16 17:58:53 +0800

    o Update for pipe-reader.c

--- pipe-reader.c
+++ pipe-reader.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <string.h>
 
 #define MAX_BUF 1024
 
@@ -9,13 +10,21 @@ int
 main()
 {
         int fd;
-        char * myfifo = "/tmp/myfifo";
+        char *myfifo = "/tmp/myfifo";
         char buf[MAX_BUF];
+        struct stat sb;
 
         /* open, read, and display the message from the FIFO */
+        printf("%d\n", stat(myfifo, &sb));
+        if (stat(myfifo, &sb) == -1) {
+                perror("stat");
+                return -1;
+        }
         fd = open(myfifo, O_RDONLY);
+        printf("The handle is %d\n", fd);
         read(fd, buf, MAX_BUF);
         printf("Received: %s\n", buf);
+        printf("Received size: %lu\n", strlen(buf));
         close(fd);
 
         return 0;
