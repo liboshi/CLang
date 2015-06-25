c0d8cb3dd1315c03eab8907f01bb5fdb4c4d0eaf
Boush Li <li.boush@gmail.com>
2015-06-25 20:08:00 +0800

    o Update.

--- pipe-writer.c
+++ pipe-writer.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -10,6 +11,7 @@ main() {
         mkfifo(myfifo, 0666);
 
         fd = open(myfifo, O_WRONLY);
+        printf("The handle is %d", fd);
         write(fd, "Hi", sizeof("Hi"));
         close(fd);
 
