34f5339cc135bbdea8e11074c60497883f010f36
Boush Li <li.boush@gmail.com>
2015-08-15 14:56:00 +0800

    o Add test for array and string.

---
+++ array-string-test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main(int argc, char *argv[])
+{
+        int i = 0;
+        char *states[] = {
+                "California", "Oregon",
+                "Washington", "Texas"
+        };
+
+        int num_states = 4;
+        for (i = 0; i < num_states; i++) {
+                printf("state %d: %s\n", i, states[i]);
+        }
+
+        return 0;
+}
