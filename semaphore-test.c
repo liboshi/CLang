bd557bade8709eec609377bbeb92a37ae642eba1
Boush Li <li.boush@gmail.com>
2015-09-02 11:18:58 +0800

    o Update test for semaphore on MacOS.

--- semaphore-test.c
+++ semaphore-test.c
@@ -24,16 +24,21 @@ void threadfunc() {
 #endif
                 printf("Hello from da thread!\n");
 #ifdef __APPLE__
-                dispatch_semaphore_release(sem);
-#endif
+                dispatch_release(sem);
+#else
                 sem_post(&sem);
+#endif
                 sleep(1);
         }
 }
 
 int main(void) {
         // initialize semaphore, only to be used with threads in this process, set value to 1
+#ifdef __APPLE__
+        sem = dispatch_semaphore_create(1);
+#else
         sem_init(&sem, 0, 1);
+#endif
         pthread_t *mythread;
 
         mythread = (pthread_t *)malloc(sizeof(*mythread));
@@ -41,12 +46,20 @@ int main(void) {
         printf("Starting thread, semaphore is unlocked.\n");
         pthread_create(mythread, NULL, (void*)threadfunc, NULL);
         getchar();
+#ifdef __APPLE__
+        dispatch_semaphore_wait(sem, DISPATCH_TIME_FOREVER);
+#else
         sem_wait(&sem);
+#endif
         printf("Semaphore locked.\n");
         // do stuff with whatever is shared between threads
         getchar();
         printf("Semaphore unlocked.\n");
+#ifdef __APPLE__
+        dispatch_release(sem);
+#else
         sem_post(&sem);
+#endif
         getchar();
         return 0;
 }
