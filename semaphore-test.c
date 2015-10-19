63f6ca4f2c7dcacb763be46cf936e060402bac4b
Boush Li <li.boush@gmail.com>
2015-10-19 23:39:05 +0800

    o Update test for semaphore on Linux.

--- semaphore-test.c
+++ semaphore-test.c
@@ -1,65 +1,32 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <pthread.h>
 #include <unistd.h>
-#ifdef __APPLE__
-#include <dispatch/dispatch.h>
-#else
+#include <fcntl.h>
 #include <semaphore.h>
-#endif
+#include <sys/stat.h>
+#include <sys/types.h>
 
-#ifdef __APPLE__
-        dispatch_semaphore_t sem;
-#else
-        sem_t sem;
-#endif
+sem_t g_sema;
 
-void threadfunc() {
-        while (1) {
-#ifdef __APPLE__
-                dispatch_semaphore_wait(sem, DISPATCH_TIME_FOREVER);
-#else
-                sem_wait(&sem);
-#endif
-                printf("Hello from da thread!\n");
-#ifdef __APPLE__
-                dispatch_release(sem);
-#else
-                sem_post(&sem);
-#endif
-                sleep(1);
-        }
+void threadfunc()
+{
+        printf("Hello from threadfunc!\n");
+        sleep(3);
+        sem_post(&g_sema);
 }
 
-int main(void) {
-        // initialize semaphore, only to be used with threads in this process, set value to 1
-#ifdef __APPLE__
-        sem = dispatch_semaphore_create(1);
-#else
-        sem_init(&sem, 0, 1);
-#endif
+int
+main()
+{
         pthread_t *mythread;
-
+        // Init semaphore
+        sem_init(&g_sema, 0, 0);
         mythread = (pthread_t *)malloc(sizeof(*mythread));
-        // start the thread
-        printf("Starting thread, semaphore is unlocked.\n");
         pthread_create(mythread, NULL, (void*)threadfunc, NULL);
-        getchar();
-#ifdef __APPLE__
-        dispatch_semaphore_wait(sem, DISPATCH_TIME_FOREVER);
-#else
-        sem_wait(&sem);
-#endif
+	sem_wait(&g_sema);
         printf("Semaphore locked.\n");
-        // do stuff with whatever is shared between threads
-        getchar();
-        printf("Semaphore unlocked.\n");
-#ifdef __APPLE__
-        dispatch_release(sem);
-#else
-        sem_post(&sem);
-#endif
-        getchar();
         return 0;
 }
+
