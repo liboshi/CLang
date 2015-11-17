1e838ffdafeb88d1e6fa0884f52d65fcc4f83ea1
Boush Li <li.boush@gmail.com>
2015-11-17 10:59:02 +0800

    o Update test for time, add get_current_time_ms function.

--- timeb-test.c
+++ timeb-test.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <sys/timeb.h>
+#include <sys/time.h>
+
+long
+get_current_time_ms()
+{
+        struct timeval tv;
+        gettimeofday(&tv, NULL);
+
+        return tv.tv_sec * 1000000 + tv.tv_usec;
+}
 
 int
 main() {
@@ -8,5 +20,6 @@ main() {
         printf("time: %hu\n", ftm.millitm);
         ftime(&ftm);
         printf("time: %hu\n", ftm.millitm);
+        printf("%ld\n", get_current_time_ms());
         return 0;
 }
