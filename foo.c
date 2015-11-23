2c9652401a9bbb0296b878406d8cc6d4cb7ca612
Boush Li <li.boush@gmail.com>
2015-11-23 22:09:25 +0800

    o Add a test for shared library.

---
+++ foo.c
@@ -0,0 +1,7 @@
+
+#include <stdio.h>
+
+void foo()
+{
+        puts("Hello, this is a shared library.");
+}
---
+++ shared-library-test.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+#include "foo.h"
+
+int main()
+{
+        puts("This is a test for shared library..,");
+        foo();
+        return 0;
+}
