8ce88d92adc8d4e086fb72acff0a10adfba33ff6
Boush Li <li.boush@gmail.com>
2015-12-03 11:15:42 +0800

    o Update test for timeb.

--- timeb-test.c
+++ timeb-test.c
@@ -10,6 +10,7 @@ get_current_time_ms()
         struct timeval tv;
         gettimeofday(&tv, NULL);
 
+        printf(">>> %ld\n", (tv.tv_sec * 1000000 + tv.tv_usec) / 1000);
         return tv.tv_sec * 1000000 + tv.tv_usec;
 }
 
@@ -17,9 +18,10 @@ int
 main() {
         struct timeb ftm;
         ftime(&ftm);
-        printf("time: %hu\n", ftm.millitm);
+        //printf("time: %hu\n", ftm.millitm);
         ftime(&ftm);
-        printf("time: %hu\n", ftm.millitm);
+        //printf("time: %hu\n", ftm.millitm);
+        printf("%f\n", get_current_time_ms());
         printf("%f\n", get_current_time_ms() / 1000);
         return 0;
 }
