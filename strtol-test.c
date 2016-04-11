6c2834abf0bb637b2bd85694189e1a7686e4ea31
Boush Li <li.boush@gmail.com>
2016-04-11 19:56:30 +0800

    o Add test for strtol.

---
+++ strtol-test.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+        char szNumbers[] = "2016 60c0c0 -1101110100110100100000 0x6fffff";
+        char *pEnd;
+        long int li1, li2, li3, li4;
+        li1 = strtol(szNumbers, &pEnd, 10);
+        li2 = strtol(pEnd, &pEnd, 16);
+        li3 = strtol(pEnd, &pEnd, 2);
+        li4 = strtol(pEnd, NULL, 0);
+        printf("The decimal equivalents are: %ld, %ld, %ld, %ld.\n", li1, li2,
+               li3, li4);
+        return 0;
+}
